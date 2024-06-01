11cf4d23d3ffa55a3079a968c03d69e8ad6ae403
Morfinix <morfinic@proton.me>
2024-06-01 20:43:54 +0200

    Dodano brakujące pliki

---
+++ app/src/main/java/com/example/projektandroid/data/BluetoothMessageMapper.kt
@@ -0,0 +1,18 @@
+package com.example.projektandroid.data
+
+import com.example.projektandroid.chat.BluetoothMessage
+
+fun String.toBluetoothMessage(isFromLocalUser: Boolean): BluetoothMessage {
+    val name = substringBeforeLast("#")
+    val message = substringAfter("#")
+
+    return BluetoothMessage(
+        message = message,
+        senderName = name,
+        isFromLocalUser = isFromLocalUser
+    )
+}
+
+fun BluetoothMessage.toByteArray(): ByteArray {
+    return "$senderName#$message".encodeToByteArray()
+}
